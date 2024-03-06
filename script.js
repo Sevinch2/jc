c048c5a373ce5619206eb72fa85f7dca73ff0859
WaLkeeD <ilnaresanov2002@gmail.com>
2024-03-06 10:56:22 +0500

    jc

---
+++ script.js
@@ -0,0 +1,34 @@
+var name = prompt("Ismingizni kiriting")
+alert(name)
+var name = prompt("Yoshingizni kiriting")
+alert(name)
+var name = prompt("100+50");
+alert(name)
+var name = prompt('150+150');
+alert(name)
+var name = prompt('400+100');
+alert(name)
+var name = prompt('500-100');
+alert(name)
+var name = prompt('450-50');
+alert(name)
+var name = prompt('300-50');
+alert(name)
+var name = prompt('5*5');
+alert(name)
+var name = prompt('6*6');
+alert(name)
+var name = prompt('7*4');
+alert(name)
+var name = prompt('10/2');
+alert(name)
+var name = prompt('50/2');
+alert(name)
+var name = prompt('100/5');
+alert(name)
+var name = prompt('13%3');
+alert(name)
+var name = prompt('15%7');
+alert(name)
+var name = prompt('17%3');
+alert(name)
